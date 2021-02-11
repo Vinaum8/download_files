6191c6d619b8304bf69dd97bc8cf6d3f926728e2
Vinicius Fernandes <vinicius.fernandes@USEALL.CORP>
2021-02-11 17:07:34 -0300

    add modulo args pra passar parametros

--- downloadfile.py
+++ downloadfile.py
@@ -1,21 +1,19 @@
 import requests
 import os
+import argparse
 
-def DownloadFile(url, local_filename):
-    local_filename = url.split('/')[-1]
+def DownloadFile(url, local):
+    nome_arquivo = os.path.join(local, url.split('/')[-1])
     with requests.get(url, stream=True) as resposta:
         resposta.raise_for_status()
-        with open(local_filename, 'wb') as file:
+        with open(nome_arquivo, 'wb') as file:
             for pedaco in resposta.iter_content(chunk_size=8096):
                 file.write(pedaco)
                 file.flush()
                 os.fsync(file.fileno())
-    return local_filename
 
-
-if __name__ == "__main__":
-    # Descomentar e colocar uma URL aqui =P
-    # BASE_URL = 'COLOCAR UMA URL AQUI'
-    OUTPUT_DIR = 'output'
-    nome_arquivo = os.path.join(OUTPUT_DIR, 'bkp_mensageria.gz')
-    DownloadFile(BASE_URL, nome_arquivo)
+parser = argparse.ArgumentParser()
+parser.add_argument("--url", help="digite a url que você deseja fazer download")
+parser.add_argument("--filepath", help="digite o local que você deseja salvar")
+args = parser.parse_args()
+DownloadFile(args.url, args.filepath)
